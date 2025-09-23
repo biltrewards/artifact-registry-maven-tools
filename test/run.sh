c860eae57dbdab575050e8e9e8e722ac25831d3a
Ken Kania <ken@bilt.com>
2025-09-23 06:56:06 -0400

    .

--- test/run.sh
+++ test/run.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ##### BEGIN CONFIG
 ###########################################################################
 
-TEST_DIR=$SCRIPT_DIR/${TEST:-maven-100-artifacts}
+TEST_DIR=$SCRIPT_DIR/${TEST:-maven-100-artifacts/virtual}
 
 # MVN_IMAGE=maven:3.9.11-eclipse-temurin-21-alpine
 # 3.8.7-eclipse-temurin-17-alpine
@@ -26,12 +26,9 @@ RUN_DIR=${RUN_DIR:-$SCRIPT_DIR/../target/tests/$(date +%s)}
 
 function run_test() {
     run_dir=$1
-    if [[ -f "$run_dir"/.mvn/extensions.xml ]]; then
-        sed -i 's/<version>[^<]*<\/version>/<version>2\.2\.6-SNAPSHOT<\/version>/' "$run_dir"/.mvn/extensions.xml
-    fi
-
-    cmd="mvn $MVN_ACTION -B -Dmaven.test.skip=true -Dmaven.repo.local=/src/_repo $MVN_FLAGS"
-    if [[ -n "MVN_IMAGE" ]]; then
+    repo_dir=$2
+    cmd="mvn $MVN_ACTION -B -Dmaven.test.skip=true $MVN_FLAGS"
+    if [[ -n "$MVN_IMAGE" ]]; then
         if [[ -n "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
             creds="-v $GOOGLE_APPLICATION_CREDENTIALS:/root/.config/gcloud/application_default_credentials.json:ro"
             creds="$creds -e GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json"
@@ -43,9 +40,13 @@ function run_test() {
             $creds \
             -w /src \
             $MVN_IMAGE \
-            $cmd" 
+            $cmd -Dmaven.repo.local=/src/$(basename $repo_dir)"
+    else
+        cd $run_dir
+        cmd="$cmd -Dmaven.repo.local=$repo_dir"
     fi
 
+    echo "RUNNING $cmd"
     $cmd | tee -a "$run_dir"/test.log
 }
 
@@ -54,5 +55,5 @@ repo_dir="$RUN_DIR"/_repo
 
 (cd $SCRIPT_DIR/.. && ./gradlew publishToMavenLocal -Dmaven.repo.local="$repo_dir")
 
-cp -rf $TEST_DIR/* "$RUN_DIR"
-run_test "$RUN_DIR"
+cp -rf $TEST_DIR/. "$RUN_DIR"
+run_test "$RUN_DIR" "$repo_dir"
